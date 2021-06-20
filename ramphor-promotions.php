020cbbef23e0d60b7de2a108258393cf37880452
Puleeno Nguyen <puleeno@gmail.com>
2021-06-20 14:36:54 +0700

    Added plugin metadata

---
+++ ramphor-promotions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Ramphor Promotions
+ * Plugin URI: https://github.com/ramphor/promotions
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: Create promotions for products, services, etc
+ */
+
